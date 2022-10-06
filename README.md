6f0f771723a683cf76c82a60750f64fda43675bc
ronnie96 <ronaldwilbourne@gmail.com>
2022-10-06 09:22:57 -0400

    2nd

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # MbogaShop
+it is a small grocery shop for my mom back in Kenya
+the products
+===========
+-
+-
+-
+-
+-
+-
+-
+-
+
+technology used
+===============
--- server.js
+++ server.js
@@ -3,8 +3,17 @@ const express = require('express')
 
 //create express app object
 const app = express()
+
+//identify your port
+const port = 3000
 // Define a "root" route directly on app
 // Tomorrow, we'll use best practice routing
+//req= express request object
+//res= response object (req,res are invoked during callbacks)
+//res.send is the route handler(callback)/otherways can be res.render-render template and send resulting html to browser
+//res.redirect tell browser to issue another GET request
+//res.json()- send a JSON response (used when we build an API)
+app.set('views','./views')
 app.get('/', function (req, res) {
     res.send('<h1>Hello World!</h1>');
   });
